aa3eeb6281b84514c2a9ebb8bcf27589a9f6a651
Jefferson Vieira da Silva <jefferson98.vieira@gmail.com>
2026-03-02 16:54:36 -0300

    feat(backend): setup envs

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello world!");
+import Fastify from 'fastify';
+
+import { env } from './config/env.ts';
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get('/', async function handler(request, reply) {
+  return { hello: 'world' };
+});
+
+try {
+  await fastify.listen({ port: env.PORT });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
