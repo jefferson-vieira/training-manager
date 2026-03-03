7468f27e488519edd507e30c3d1ebf4684948407
Jefferson Vieira da Silva <jefferson98.vieira@gmail.com>
2026-03-02 21:41:17 -0300

    feat(backend): setup swagger

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,7 +1,11 @@
 import type { ZodTypeProvider } from 'fastify-type-provider-zod';
 
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUI from '@fastify/swagger-ui';
 import Fastify from 'fastify';
 import {
+  createJsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod';
@@ -16,23 +20,47 @@ const app = Fastify({
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.withTypeProvider<ZodTypeProvider>().route({
-  handler: (req, res) => {
-    res.send(req.query.name);
-  },
-  method: 'GET',
-  schema: {
-    querystring: z.object({
-      name: z.string().min(4),
-    }),
-    response: {
-      200: z.string(),
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'training-manager API',
+      version: '1.0.0',
     },
+    servers: [
+      {
+        description: 'localhost',
+        url: `http://localhost:${env.PORT}`,
+      },
+    ],
   },
-  url: '/',
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/documentation',
+});
+
+app.after(() => {
+  app.withTypeProvider<ZodTypeProvider>().route({
+    handler: (req, res) => {
+      res.send(req.query.name);
+    },
+    method: 'GET',
+    schema: {
+      querystring: z.object({
+        name: z.string().min(4),
+      }),
+      response: {
+        200: z.string(),
+      },
+    },
+    url: '/',
+  });
 });
 
 try {
+  await app.ready();
+
   await app.listen({ port: env.PORT });
 } catch (err) {
   app.log.error(err);
