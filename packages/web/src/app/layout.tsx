1cf526e7554bebe92e90ecdd3b26f9a1e73b9269
Jefferson Vieira da Silva <jefferson98.vieira@gmail.com>
2026-03-08 11:23:04 -0300

    chore(web): setup shadcn-ui

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next';
 
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Geist_Mono, Inter } from 'next/font/google';
 
 import './globals.css';
+import { cn } from '@/lib/utils';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 const geistSans = Geist({
   subsets: ['latin'],
@@ -25,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className={cn('font-sans', inter.variable)} lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
