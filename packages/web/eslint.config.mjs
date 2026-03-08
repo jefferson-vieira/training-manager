aac7f1183e765a84e320ac2c9b97c0021bb30d2c
Jefferson Vieira da Silva <jefferson98.vieira@gmail.com>
2026-03-07 23:01:43 -0300

    chore(web): setup lint

--- packages/web/eslint.config.mjs
+++ packages/web/eslint.config.mjs
@@ -1,18 +1,42 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import stylistic from '@stylistic/eslint-plugin';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import perfectionist from 'eslint-plugin-perfectionist';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  stylistic.configs.recommended,
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/padding-line-between-statements': [
+        'error',
+        {
+          blankLine: 'always',
+          next: '*',
+          prev: ['const', 'let', 'var', 'directive', 'import'],
+        },
+        {
+          blankLine: 'always',
+          next: ['if', 'for', 'while', 'switch', 'try', 'return'],
+          prev: '*',
+        },
+        {
+          blankLine: 'any',
+          next: ['const', 'let', 'var', 'import'],
+          prev: ['const', 'let', 'var', 'import'],
+        },
+      ],
+    },
+  },
+  eslintPluginPrettierRecommended,
+  perfectionist.configs['recommended-alphabetical'],
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
 ]);
 
 export default eslintConfig;
